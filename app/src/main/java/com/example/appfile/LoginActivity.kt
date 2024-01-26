82b1257e347a9d0cafd9bb2989b0820808f0feb8
unigcorp <unigsystem@gmail.com>
2024-01-25 21:11:34 -0400

    1/25/2024

--- app/src/main/java/com/example/appfile/LoginActivity.kt
+++ app/src/main/java/com/example/appfile/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.example.appfile
 
+import android.app.Dialog
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.util.PatternsCompat
 import com.android.volley.Request
@@ -20,8 +24,19 @@ import org.json.JSONObject
 class LoginActivity : AppCompatActivity() {
     private lateinit var correoEditText:EditText
     private lateinit var contrasenaEditText:EditText
+    private lateinit var btn_forgotPassword:TextView
     private lateinit var btnSend:Button
+    private lateinit var btnRegister:Button
     private  lateinit var sharedPreferences:SharedPreferences
+
+    //DIALOG
+    private lateinit var editTextCorreo:EditText
+    private lateinit var btn_envia:Button
+    private lateinit var btn_cancela:Button
+
+
+    private var correoRecibido=""
+    private var contrasenaRecibido=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +46,11 @@ class LoginActivity : AppCompatActivity() {
         correoEditText = findViewById(R.id.id_etCorreo)
         contrasenaEditText = findViewById(R.id.id_etContrasena)
         btnSend = findViewById(R.id.id_btn_send)
+        btnRegister = findViewById(R.id.btn_register)
+        btn_forgotPassword = findViewById(R.id.id_forgotPassword)
+
+
+        //correoRecibido = getEx
 
         btnSend.setOnClickListener {
             val strCorreo = correoEditText.text.toString()
@@ -43,8 +63,72 @@ class LoginActivity : AppCompatActivity() {
 
 
         }
+        btnRegister.setOnClickListener {
+            //startActivity(Intent(this,SignupActivity::class.java))
+            val intent = Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+        }
+        btn_forgotPassword.setOnClickListener {
+            openDialod()
+        }
 
 
+
+    }
+
+    private fun openDialod() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_forgotpassword)
+        val width = ViewGroup.LayoutParams.MATCH_PARENT
+        val heigth = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog.window?.setLayout(width,heigth)
+
+
+        editTextCorreo = dialog.findViewById(R.id.editTextCorreo)
+        btn_envia = dialog.findViewById(R.id.btn_envia)
+        btn_cancela = dialog.findViewById(R.id.btn_cancela)
+
+
+        btn_cancela.setOnClickListener {
+            dialog.dismiss()
+        }
+        btn_envia.setOnClickListener {
+            enviaDatos(editTextCorreo.text.toString())
+        }
+
+
+
+        dialog.show()
+    }
+
+    private fun enviaDatos(correo: String) {
+        val queque = Volley.newRequestQueue(this)
+        val parametros = JSONObject()
+
+        parametros.put("correo",correo)
+
+        val strRequest = JsonObjectRequest(
+            Request.Method.POST,Contants.ADRRESS_IP+"user/forgotPAssword",parametros, {
+                    response ->
+                try {
+
+                    Toast.makeText(this, "SE ENVIO SU CONTRASENA A SU CORREO", Toast.LENGTH_SHORT).show()
+
+                    finish()
+
+                }   catch (e: JSONException){
+                    Toast.makeText(this, "Error de exception"+e.message, Toast.LENGTH_SHORT).show()
+                }
+
+            },
+            Response.ErrorListener {
+                    error ->
+                if (error.networkResponse.statusCode==400){
+                    Toast.makeText(this, "EL CORREO NO EXISTE", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        queque.add(strRequest)
 
     }
 
@@ -71,11 +155,18 @@ class LoginActivity : AppCompatActivity() {
                  val y = x.getString("nombre")*/
 
                  val token = response.getString("token")
+                 val data = response.getJSONArray("result")
+                 val x = data.getJSONObject(0)
+                 val nombre = x.getString("nombre")
+                 val id_usuario = x.getString("id_usuario")
+                 saveShared(token,id_usuario,nombre)
                  //val dato = response.getJSONObject("")
                  //Toast.makeText(this, "==== "+token+" ", Toast.LENGTH_SHORT).show()
                  //saveShared(token)
                  //Toast.makeText(this, "==="+y, Toast.LENGTH_SHORT).show()
-                 Toast.makeText(this, "==="+token.toString(), Toast.LENGTH_SHORT).show()
+                 Toast.makeText(this, "INICIO DE SESION CON EXITO", Toast.LENGTH_SHORT).show()
+                 startActivity(Intent(this,MainActivity::class.java))
+
              }   catch (e:JSONException){
                  Toast.makeText(this, "Error de exception"+e.message, Toast.LENGTH_SHORT).show()
              }
@@ -86,9 +177,11 @@ class LoginActivity : AppCompatActivity() {
         queque.add(strRequest)
     }
 
-    private fun saveShared(token: String) {
+    private fun saveShared(token: String,id:String,nombre:String) {
         val  sharedEdit = sharedPreferences.edit()
         sharedEdit.putString("TOKEN",token)
+        sharedEdit.putString("ID",id)
+        sharedEdit.putString("NOMBRE",nombre)
         sharedEdit.apply()
     }
     private fun getToken():String?{
--- app/src/main/java/com/example/appfile/SignupActivity.kt
+++ app/src/main/java/com/example/appfile/SignupActivity.kt
@@ -1,11 +1,99 @@
 package com.example.appfile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.util.PatternsCompat
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var edtNombre:EditText
+    private lateinit var edtApellidos:EditText
+    private lateinit var edtCorreo:EditText
+    private lateinit var edtContrasena:EditText
+    private lateinit var btn_registrar:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        edtNombre = findViewById(R.id.id_etNombres)
+        edtApellidos = findViewById(R.id.id_etApellidos)
+        edtCorreo = findViewById(R.id.id_etCorreo)
+        edtContrasena = findViewById(R.id.id_etContrasena)
+        btn_registrar = findViewById(R.id.btn_registrar)
+        btn_registrar.setOnClickListener {
+            validarDatos(edtNombre.text.toString(),edtApellidos.text.toString(),edtCorreo.text.toString(),edtContrasena.text.toString())
+        }
+    }
+
+    private fun validarDatos(strnombre: String, strapellido: String, strcorreo: String, strcontrasena: String) {
+        if(!TextUtils.isEmpty(strnombre)||!TextUtils.isEmpty(strapellido)||!TextUtils.isEmpty(strcorreo)||!TextUtils.isEmpty(strcontrasena)){
+            if(!PatternsCompat.EMAIL_ADDRESS.matcher(strcorreo).matches()){
+                Toast.makeText(this, "INSERTA UN CORREO VALIDO", Toast.LENGTH_SHORT).show()
+            }else{
+                if(strcontrasena.length>=6){
+                    enviaDatos(strnombre,strapellido,strcorreo,strcontrasena)
+                }else{
+                    Toast.makeText(this, "INSERTA UNA CONTRASENA VALIDA", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }else{
+            Toast.makeText(this, "DEBE COMPLETAR TODOS LOS CAMPOS", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun enviaDatos(strnombre: String, strapellido: String, strcorreo: String, strcontrasena: String) {
+        val queque = Volley.newRequestQueue(this)
+        val parametros = JSONObject()
+        parametros.put("nombre",strnombre)
+        parametros.put("apellidos",strapellido)
+        parametros.put("correo",strcorreo)
+        parametros.put("contrasena",strcontrasena)
+
+        val strRequest = JsonObjectRequest(
+            Request.Method.POST,"http://192.168.0.15:8080/user/signup",parametros, {
+                response ->
+            try {
+
+                Toast.makeText(this, "REGISTRO CON EXITO", Toast.LENGTH_SHORT).show()
+                vaciarDatos(edtNombre,edtApellidos,edtCorreo,edtContrasena)
+                //startActivity(Intent(this,LoginActivity::class.java))
+                val intent = Intent(this,LoginActivity::class.java)
+                intent.putExtra("correo",strcorreo)
+                intent.putExtra("contrasena",strcontrasena)
+                startActivity(intent)
+                finish()
+
+            }   catch (e: JSONException){
+                Toast.makeText(this, "Error de exception"+e.message, Toast.LENGTH_SHORT).show()
+            }
+
+        },
+            Response.ErrorListener {
+                    error ->
+                if (error.networkResponse.statusCode==400){
+                    Toast.makeText(this, "EL CORREO QUE QUIERE REGISTRAR YA EXISTE", Toast.LENGTH_SHORT).show()
+                }
+
+        })
+        queque.add(strRequest)
+    }
+
+    private fun vaciarDatos(edtNombre: EditText, edtApellidos: EditText, edtCorreo: EditText, edtContrasena: EditText) {
+        edtNombre.setText("")
+        edtApellidos.setText("")
+        edtCorreo.setText("")
+        edtContrasena.setText("")
     }
 }
